366bb4ed553a61399612be64a57535fce19cf86c
Hrishabh3829 <2200032748@kluniversity.in>
2025-08-13 12:20:12 +0530

    isLoading

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,13 +6,36 @@ import { Provider } from "react-redux";
 import { appStore } from "./app/store";
 import { Toaster } from "sonner";
 import { ThemeProvider } from "next-themes";
+import { useLoadUserQuery } from "./features/api/authApi";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+
+const Custom = ({ children }) => {
+  const { isLoading } = useLoadUserQuery();
+  // const { isLoading } = { isLoading: true }; 
+
+  return (
+    <>
+      {isLoading ? 
+        <DotLottieReact
+          src="https://lottie.host/99307f19-5bee-48c9-90f1-11858c3a98d1/vtpTJ34roC.lottie"
+          loop
+          autoplay
+        />
+       : 
+        <>{children}</>
+      }
+    </>
+  );
+};
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={appStore}>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        <App />
-        <Toaster />
+        <Custom>
+          <App />
+          <Toaster />
+        </Custom>
       </ThemeProvider>
     </Provider>
   </StrictMode>
