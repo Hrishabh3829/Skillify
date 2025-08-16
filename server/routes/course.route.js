1e5b11e125c8ffaf61fcb4aca0dd843ad6b92e8a
Hrishabh3829 <2200032748@kluniversity.in>
2025-08-16 19:55:03 +0530

    editCourse api logic done

--- server/routes/course.route.js
+++ server/routes/course.route.js
@@ -1,11 +1,18 @@
 import express from "express";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
-import { createCourse, getCreatorCourses } from "../controller/course.controller.js";
+import {
+  createCourse,
+  editCourse,
+  getCreatorCourses,
+} from "../controller/course.controller.js";
+import upload from "../utils/multer.js";
 
 const router = express.Router();
 
-router.route("/").post(isAuthenticated,createCourse)
-router.route("/").get(isAuthenticated,getCreatorCourses)
-
+router.route("/").post(isAuthenticated, createCourse);
+router.route("/").get(isAuthenticated, getCreatorCourses);
+router
+  .route("/:courseId")
+  .put(isAuthenticated, upload.single("courseThumbnail"), editCourse);
 
 export default router;
