34ec0fe16a8635fda20acfeeccfdcac44c4c9c5e
Hrishabh3829 <2200032748@kluniversity.in>
2025-09-01 22:13:14 +0530

    populate enrolled courses

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -145,7 +145,10 @@ export const getUserProfile = async (req, res) => {
     const userId = req.id;
     const user = await User.findById(userId)
       .select("-password")
-      .populate("enrolledCourses");
+      .populate({
+        path: "enrolledCourses",
+        populate: { path: "creator", select: "name photoUrl" },
+      });
       
     if (!user) {
       return res.status(404).json({
